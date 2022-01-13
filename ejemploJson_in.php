ee72d4bd83709ceb08b7f42ac3dfea39e603bb59
hugomunozsantana <95626271+hugomunozsantana@users.noreply.github.com>
2022-01-13 20:54:57 -0300

    Create ejemploJson_in.php

---
+++ ejemploJson_in.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<?php
+$json=$_GET['x'];
+?>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<h1> mi php recibe json</h1><br>
+	<p>
+		<?php echo "json:".$json; ?>
+	</p>
+</body>
+</html>
